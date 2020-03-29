3c4b8380f2f473ce6bafdcb451f18f1d528413a4
yumran <2506772508@qq.com>
2020-03-29 15:59:37 +0800

    定义变量

--- src/main/java/SpringBoot/controller/HelloController.java
+++ src/main/java/SpringBoot/controller/HelloController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public String hello(){
+        int i=0;
         return "hello spring boot";
     }
 }
